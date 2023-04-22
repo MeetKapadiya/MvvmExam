80b337002f90f6c32d737e6a087897613a06b90d
First Program <meetkapadiya24@gmail.com>
2023-04-22 08:18:52 +0530

    Initial Commit

---
+++ MvvmExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MvvmExam
+//
+//  Created by Meet Kapadiya on 22/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
